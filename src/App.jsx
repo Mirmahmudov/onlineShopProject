c86a5e73fbcaf65de43adf9de5da7f21d5ff3803
Asadbek <asadbekmirmahmudov3@gmail.com>
2026-01-26 16:57:49 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import ProductDetail from './pages/productDetail/ProductDetail'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/product' element={<ProductDetail />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
+
+
+    </>
+  )
+}
+
+export default App
