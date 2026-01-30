de9cff57295ec0097c33a118d9e77f2abee107e2
Nurmuhammad <nurmuhammadabdubannonov@gmail.com>
2026-01-30 16:05:01 +0500

    add

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+
+
+      <div className="product-grid">
+          <div className="product-card" >
+            <div className="discount">-30%</div>
+
+            <img src="./public/lWGeSFc0Y6jBoVxA4hQw-home_default 1.svg" alt="product" />
+
+            <p className="product-name">TECNO Spark 6 Go 3/64GB</p>
+
+            <div className="price">
+              <span className="old">1 300 000 сум</span>
+              <span className="new">1 040 000 сум</span>
+            </div>
+
+            <div className="icons">
+              <button>♡</button>
+              <button>🛒</button>
+            </div>
+          </div>
+      </div>
+  );
+}
+
+export default Product;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import Category from "../../components/category/Category";
+import Product from "../../components/product/Product";
+import Swiper from "../../components/swiper/Swiper";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="hero">
+        <div className="container">
+          <Swiper />
+        </div>
+      </div>
+      <main>
+        <section className="one">
+          <div className="container">
+            <div className="sarlavha">
+              <h1>Рекомендуем</h1>
+              <span>Посмотреть все →</span>
+            </div>
+            <Product />
+          </div>
+        </section>
+        <section className="two">
+          <div className="container">
+            <div className="sarlavha">
+              <h1>Popular category</h1>
+            </div>
+            <Category />
+          </div>
+        </section>
+        <section className="three">
+          <div className="container">
+            <div className="sarlavha">
+              <h1>Рекомендуем</h1>
+              <span>Посмотреть все →</span>
+            </div>
+            <div className="recomend">
+              <img src="./public/newHotRus 1.svg" alt="" />
+              <Product />
+            </div>
+          </div>
+        </section>
+        <section className="four">
+          <div className="container">
+            <div className="card">
+              <img src="./public/Layer 48 copy 1.svg" alt="" />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default Home
+export default Home;
