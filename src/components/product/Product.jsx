d4d9c8349256a96bfebf2b072f02535fa9211e99
Nurmuhammad <nurmuhammadabdubannonov@gmail.com>
2026-02-02 15:30:25 +0500

    add

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import "./Product.css";
+import { useNavigate } from "react-router-dom";
 
 function Product() {
+  const navigate = useNavigate();
+
   return (
+    <div className="product-grid" onClick={() => navigate("/ProductDetail")}>
+      <div className="product-card">
+        <div className="discount">-30%</div>
 
+        <img src="/lWGeSFc0Y6jBoVxA4hQw-home_default 1.svg" alt="product" />
 
-      <div className="product-grid">
-          <div className="product-card" >
-            <div className="discount">-30%</div>
+        <p className="product-name">TECNO Spark 6 Go 3/64GB</p>
 
-            <img src="./public/lWGeSFc0Y6jBoVxA4hQw-home_default 1.svg" alt="product" />
+        <div className="price">
+          <span className="old">1 300 000 сум</span>
+          <span className="new">1 040 000 сум</span>
+        </div>
 
-            <p className="product-name">TECNO Spark 6 Go 3/64GB</p>
-
-            <div className="price">
-              <span className="old">1 300 000 сум</span>
-              <span className="new">1 040 000 сум</span>
-            </div>
-
-            <div className="icons">
-              <button>♡</button>
-              <button>🛒</button>
-            </div>
-          </div>
+        <div className="icons">
+          <button>♡</button>
+          <button>🛒</button>
+        </div>
       </div>
+    </div>
   );
 }
 
--- src/pages/productDetail/ProductDetail.jsx
+++ src/pages/productDetail/ProductDetail.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import React from "react";
+// import "ProductDetail.css";
+import { FaRegHeart } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { GiScales } from "react-icons/gi";
 function ProductDetail() {
   return (
-    <div>ProductDetail</div>
-  )
+    <>
+      <div className="container">
+        <div className="info">
+          <div className="imgs">
+            <img src="./public/Rectangle 216.svg" alt="" />
+            <div className="smallimg">
+              <img src="./public/Rectangle 216.svg" alt="" />
+              <img src="./public/Rectangle 216.svg" alt="" />
+              <img src="./public/Rectangle 216.svg" alt="" />
+              <img src="./public/Rectangle 216.svg" alt="" />
+            </div>
+          </div>
+          <h1>MacBook Pro 13 MXK32ZP/A Space Gray</h1>
+          <h3>16 559 000 cум</h3>
+          <div className="icons">
+            <FiShoppingCart />
+            <FaRegHeart />
+            <GiScales />
+          </div>
+          <div className="btns">
+            <button>Купить сейчас</button>
+            <button>Купить в рассрочку сейчас</button>
+          </div>
+          <p>
+            {" "}
+            MacBook Pro 13 MXK32ZP/A Space Gray Full HD 1920x1080 IPS / Core™
+            i7-1165G7 / 8GB RAM / 256GB SSD
+          </p>
+        </div>
+        <h1>Технические параметры</h1>
+        <div className="sec">
+          <h2>Название</h2>
+          <p>MacBook Pro 13 MXK32ZP/A Space Gray</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ProductDetail
+export default ProductDetail;
